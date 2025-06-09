fb5dded374f72db7b8e2f8fbfc2028f25c2a1d33
JCDC343 <diaz.j@maristcollege.school.nz>
2025-06-09 12:07:29 +1200

    Added action functions, with stats updates

--- src/comp603_group_project_2/Cat.java
+++ src/comp603_group_project_2/Cat.java
@@ -12,4 +12,43 @@ public class Cat extends VirtualPet {
     public Cat(String name){
         super(name);
     }
+    
+    @Override
+    public void play() {
+        System.out.println(getName() + " pounces on a laser pointer.");
+        updateHappiness(25);
+        updateEnergy(-15);
+        updateHunger(-5);
+        increaseHealth(5);
+    }
+    
+    @Override
+    public void eat() {
+        System.out.println(getName() + " enjoys some fish.");
+        updateHunger(25);
+        updateEnergy(5);
+        increaseHealth(10);
+    }
+    
+    @Override
+    public void sleep() {
+        System.out.println(getName() + " curls up for a snooze. zzzz");
+        updateEnergy(30);
+        updateHunger(-5);
+        increaseHealth(5);
+    }
+    
+    @Override 
+    public void makeNoise() {
+        System.out.println("Meow Meowww!");
+    }
+    
+    @Override
+    public void clean() {
+        System.out.println(getName() + " is grooming itself.");
+        updateHappiness(10);
+        updateEnergy(-3);
+        increaseHealth(3);
+    }
+   
 }
