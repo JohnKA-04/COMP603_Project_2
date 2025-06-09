a148163a8823d101c794892f669bef5f2f41e52a
JCDC343 <diaz.j@maristcollege.school.nz>
2025-06-09 12:25:05 +1200

    Added action functions, with stats updates

--- src/comp603_group_project_2/Hamster.java
+++ src/comp603_group_project_2/Hamster.java
@@ -8,6 +8,47 @@ package comp603_group_project_2;
  *
  * @author johnk
  */
-public class Hamster {
+public class Hamster extends VirtualPet {
+    public Hamster(String name) {
+        super(name);
+    }
+    
+    @Override
+    public void play() {
+        System.out.println(getName() + " goes for a run in their hamster ball.");
+        updateHappiness(30);
+        updateEnergy(-20);
+        updateHunger(-15);
+        increaseHealth(5);
+    }
+    
+    @Override
+    public void eat() {
+        System.out.println(getName() + " nibbles on some hamster pellets.");
+        updateHunger(20);
+        updateEnergy(10);
+        increaseHealth(15);
+    }
+    
+    @Override
+    public void sleep() {
+        System.out.println(getName() + " huddles beneath some straw to doze off. zz");
+        updateEnergy(35);
+        updateHunger(-10);
+        increaseHealth(10);
+    }
+    
+    @Override
+    public void makeNoise() {
+        System.out.println("squeak squeak..");
+    }    
+    
+    @Override
+    public void clean() {
+        System.out.println(getName() + " licks their belly fur.");
+        updateHappiness(15);
+        updateEnergy(-3);
+        increaseHealth(5);
+    }
     
 }
